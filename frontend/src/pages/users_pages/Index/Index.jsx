4d63662394badbad4cbff97c25413ae02af6ad3b
Debadrita-rgb <debadritapaul76@gmail.com>
2026-03-25 00:01:15 +0530

    fix: case-sensitive import path in index page

--- frontend/src/pages/users_pages/Index/Index.jsx
+++ frontend/src/pages/users_pages/Index/Index.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import BannerSection from "../../../components/usersComponents/Banner/BannerSection";
-import MovieGallery from "../../../components/usersComponents/movieGallery/movieGallery";
+import MovieGallery from "../../../components/usersComponents/MovieGallery/MovieGallery";
 import LifeEvents from "../../../components/usersComponents/LifeEvents/LifeEvents";
 
 const VoyageHomePage = () => {
   return (
     <>
       <BannerSection />
-      < MovieGallery />
+      < MovieGallery /> 
       <LifeEvents />
     </>
   );
