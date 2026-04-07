9d96890726a20fb4a4c3d3de789316e63e688fe0
Debadrita-rgb <debadritapaul76@gmail.com>
2026-04-07 21:29:20 +0530

    fixing error for send otp

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -49,13 +49,18 @@ router.post("/send-otp", async (req, res) => {
 console.log("EMAIL:", process.env.EMAIL_USER);
 console.log("PASS:", process.env.EMAIL_PASS ? "EXISTS" : "MISSING");
 
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, 
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
     
 
     await transporter.sendMail({
